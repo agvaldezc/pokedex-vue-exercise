3f2f0c3dcb976c2f6bce556dbb55f0e7e8331490
Alan Valdez <alan.gvaldez@gmail.com>
2022-12-15 17:25:06 -0600

    Initial Commit for exercise.

--- src/components/PokedexContainer.vue
+++ src/components/PokedexContainer.vue
@@ -1,38 +1,16 @@
-<script setup>
-import PokemonCard from './PokemonCard.vue';
-import { getPokemon } from '@/services/pokedex.service';
-import { computed, ref } from 'vue';
-
-const pokemon = ref(null);
-const error = ref(null);
-const query = ref('');
-
-const isValidQuery = computed(() => /^[a-zA-z0-9]+$/.test(query.value));
-
-function searchPokemon() {
-  getPokemon(query.value)
-    .then((res) => {
-      pokemon.value = res;
-      error.value = null;
-    })
-    .catch((err) => {
-      error.value = err;
-      pokemon.value = null;
-    });
-}
-</script>
+<script setup></script>
 
 <template>
   <section className="pokedex">
-    <form @submit.prevent="searchPokemon" className="pokedex-search-form">
+    <form className="pokedex-search-form">
       <h2>Pokedex Component</h2>
       <fieldset>
-        <input v-model="query" name="query" type="text" placeholder="Pokemon name or ID" />
-        <button :disabled="!isValidQuery" type="submit">Search Pokemon</button>
+        <input name="query" type="text" placeholder="Pokemon name or ID" />
+        <button type="submit">Search Pokemon</button>
       </fieldset>
     </form>
     <article className="pokedex-container">
-      <PokemonCard :pokemon="pokemon" :error="error" />
+      <!-- Need to use the PokemonCard component. -->
     </article>
   </section>
 </template>
